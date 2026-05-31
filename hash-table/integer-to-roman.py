76c9beb806473a221c2851f7dcdfcd978f4d6fa3
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-05-31 10:59:20 +0530

    LC-integer-to-roman [Runtime: N/A; Memory: N/A]

--- hash-table/integer-to-roman.py
+++ hash-table/integer-to-roman.py
@@ -1,8 +1,17 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
 
-        values = [1000, 500, 100, 50, 10, 5, 1]
-        symbols = ["M", "D", "C", "L", "X", "V", "I"]
+        values = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        ]
+
+        symbols = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        ]
 
         res = ""
 
