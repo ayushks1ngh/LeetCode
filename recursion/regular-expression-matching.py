bdc4c639ef84c44085f439270bd2eb579710994f
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-05-31 07:23:01 +0530

    LC-regular-expression-matching [Runtime: N/A; Memory: N/A]

--- recursion/regular-expression-matching.py
+++ recursion/regular-expression-matching.py
@@ -3,24 +3,27 @@ class Solution:
 
         memo = {}
 
-        def dfs(i, j):
+        def dp(i, j):
             if (i, j) in memo:
                 return memo[(i, j)]
 
             if j == len(p):
                 return i == len(s)
 
-            first = (
+            first_match = (
                 i < len(s) and
                 (s[i] == p[j] or p[j] == '.')
             )
 
-            if first:
-                ans = dfs(i + 1, j + 1)
+            if j + 1 < len(p) and p[j + 1] == '*':
+                ans = (
+                    dp(i, j + 2) or
+                    (first_match and dp(i + 1, j))
+                )
             else:
-                ans = False
+                ans = first_match and dp(i + 1, j + 1)
 
             memo[(i, j)] = ans
             return ans
 
-        return dfs(0, 0)
+        return dp(0, 0)
