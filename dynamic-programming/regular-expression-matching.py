220ea24f3caae0cf16a64df08b178eb9245fb152
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-05-31 07:22:47 +0530

    LC-regular-expression-matching [Runtime: N/A; Memory: N/A]

--- dynamic-programming/regular-expression-matching.py
+++ dynamic-programming/regular-expression-matching.py
@@ -1,14 +1,26 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        i = j = 0
 
-        while i < len(s) and j < len(p):
-            if p[j] == '.' or s[i] == p[j]:
-                i += 1
-                j += 1
-            elif j + 1 < len(p) and p[j + 1] == '*':
-                j += 2
+        memo = {}
+
+        def dfs(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if j == len(p):
+                return i == len(s)
+
+            first = (
+                i < len(s) and
+                (s[i] == p[j] or p[j] == '.')
+            )
+
+            if first:
+                ans = dfs(i + 1, j + 1)
             else:
-                return False
+                ans = False
 
-        return i == len(s) and j == len(p)
+            memo[(i, j)] = ans
+            return ans
+
+        return dfs(0, 0)
