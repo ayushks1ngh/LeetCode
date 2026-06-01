56317e5a6467222fa8c8a7491532def7af2f6852
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-06-01 23:00:29 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.py
+++ sorting/3sum.py
@@ -1,15 +1,37 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
 
-        result = set()
-        n = len(nums)
+        nums.sort()
+        result = []
 
-        for i in range(n):
-            for j in range(i + 1, n):
-                for k in range(j + 1, n):
+        for i in range(len(nums) - 2):
 
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        triplet = tuple(sorted([nums[i], nums[j], nums[k]]))
-                        result.add(triplet)
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-        return [list(x) for x in result]
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+
+                elif total > 0:
+                    right -= 1
+
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return result
