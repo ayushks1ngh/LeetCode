bb383e89f02fd7909f03b2f10be672754743c599
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-06-02 10:29:06 +0530

    LC-generate-parentheses [Runtime: N/A; Memory: N/A]

--- string/generate-parentheses.py
+++ string/generate-parentheses.py
@@ -3,15 +3,26 @@ class Solution:
 
         result = []
 
-        def dfs(path):
+        def backtrack(path, open_count, close_count):
 
             if len(path) == 2 * n:
                 result.append(path)
                 return
 
-            dfs(path + "(")
-            dfs(path + ")")
+            if open_count < n:
+                backtrack(
+                    path + "(",
+                    open_count + 1,
+                    close_count
+                )
 
-        dfs("")
+            if close_count < open_count:
+                backtrack(
+                    path + ")",
+                    open_count,
+                    close_count + 1
+                )
+
+        backtrack("", 0, 0)
 
         return result
