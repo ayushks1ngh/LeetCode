ce55a0c8d8529c24ac7363fde4fc96475b5ca68d
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-08-04 20:56:56 +0530

    LC-maximum-subarray [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/maximum-subarray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current = best = nums[0]
+
+        for num in nums[1:]:
+            current = max(num, current + num)
+            best = max(best, current)
+
+        return best
