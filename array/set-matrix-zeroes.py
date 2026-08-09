2fd90a299a08fa1bbb6ebe6ed8e35f00d51cb719
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-08-09 23:18:47 +0530

    LC-set-matrix-zeroes [Runtime: 7 ms; Memory: 21 MB]

--- array/set-matrix-zeroes.py
+++ array/set-matrix-zeroes.py
@@ -1,21 +1,5 @@
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        # input validation
-        if not matrix:
-            return []
-
-        m = len(matrix)
-        n = len(matrix[0])
-
-         zeroes_row = [False] * m
-         zeroes_col = [False] * n
-         for row in range(m):
-             for col in range(n):
-                 if matrix[row][col] == 0:
-                     zeroes_row[row] = True
-                     zeroes_col[col] = True
-
-         for row in range(m):
-             for col in range(n):
-                 if zeroes_row[row] or zeroes_col[col]:
-                     matrix[row][col] = 0
+    def setZeroes(self, g: List[List[int]]) -> None:
+        r, c = [*map(all,g)], [*map(all,zip(*g))]
+        for i,j in product(range(len(g)),range(len(g[0]))):
+            g[i][j] *= r[i]&c[j]
