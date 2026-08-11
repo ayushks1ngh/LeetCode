ba83d27f23bcf84062ead9ffca16704b69ca15f9
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-08-11 22:31:49 +0530

    LC-largest-rectangle-in-histogram [Runtime: N/A; Memory: N/A]

--- monotonic-stack/largest-rectangle-in-histogram.py
+++ monotonic-stack/largest-rectangle-in-histogram.py
@@ -1,26 +1,23 @@
-class Solution {
-public:
-    int largestRectangleArea(vector<int>& hts) {
-        int size=hts.size();
-        int to_right[size];
-        stack<int> st;
-        for(int i=size-1;i>=0;--i){
-            while(!st.empty() and hts[st.top()]>=hts[i])
-                st.pop();
-            to_right[i]=st.empty()?size:st.top();
-            st.push(i);
-        }
-        while(!st.empty())st.pop();
-        int maxarea=0;
-        for(int i=0;i<size;++i){
-            while(!st.empty() and hts[st.top()]>=hts[i])
-                st.pop();
-            int to_left=st.empty()?-1:st.top();
-            maxarea=max(maxarea,(to_right[i]-to_left-1)*hts[i]);
-            st.push(i);
-        }
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        maxArea = 0
+        stack = []
+
+        for index , height in enumerate(heights):
+            start = index
+            
+            while start and stack[-1][1] > height:
+                i , h = stack.pop()
+                maxArea = max(maxArea , (index-i)*h)
+                start = i
+            stack.append((start , height))
+
+        for index , height in stack:
+            maxArea = max(maxArea , (len(heights)-index)*height)
+
+        return maxArea    
+
+                
+
+
         
-        return maxarea;
-        
-    }
-};
