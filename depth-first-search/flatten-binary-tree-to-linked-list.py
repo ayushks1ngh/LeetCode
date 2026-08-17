c82c98ad39b381cdbb0ff077b7ede44402025295
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-08-17 21:55:02 +0530

    LC-flatten-binary-tree-to-linked-list [Runtime: N/A; Memory: N/A]

--- depth-first-search/flatten-binary-tree-to-linked-list.py
+++ depth-first-search/flatten-binary-tree-to-linked-list.py
@@ -1,14 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def flatten(self, root: TreeNode) -> None:
-        cur = root
-        while cur:
-            if cur.left:
-                prev = cur.left
-                while prev.right:
-					prev = prev.right    # We go to left Subtree's rightMost Node
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        curr = root
+        
+        while curr:
+            if curr.left:
+                # 1. Find the 'tail' of the left subtree
+                # (The rightmost node in the left subtree)
+                predecessor = curr.left
+                while predecessor.right:
+                    predecessor = predecessor.right
                 
-                prev.right = cur.right   #We make current Node's right Subtree prev's right Subtree
-                cur.right = cur.left    # We make it right Subtree
-                cur.left = None   # Removing left 
+                # 2. HEURISTIC: Graft the current right subtree 
+                # onto the end of the left subtree's tail.
+                predecessor.right = curr.right
+                
+                # 3. Move the modified left subtree to the right
+                curr.right = curr.left
+                curr.left = None
             
-            cur = cur.right
+            # 4. Move to the next node in our growing 'Vine'
+            curr = curr.right
