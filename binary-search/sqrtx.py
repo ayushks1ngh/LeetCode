11c0d75845ddea6095d7a58531082ec1ffe58a23
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-08-29 23:30:08 +0530

    Synchronize existing submission history (2/3)

--- binary-search/sqrtx.py
+++ binary-search/sqrtx.py
@@ -3,9 +3,17 @@ class Solution(object):
         if x < 2:
             return x
         
-        i = 2
-        while i * i <= x:
-            i += 1
+        left, right = 1, x // 2
         
-        return i - 1
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+            
+            if square == x:
+                return mid
+            elif square < x:
+                left = mid + 1
+            else:
+                right = mid - 1
         
+        return right 
