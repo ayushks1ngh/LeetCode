cc83c72924a5b98e1df8e8ed8a1a7a41aac7bcfb
Ayush Kumar Singh <ayushkrsingh8015@gmail.com>
2026-09-17 18:49:17 +0530

    LC-linked-list-cycle [Runtime: N/A; Memory: N/A]

---
+++ linked-list/linked-list-cycle.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        for i in range(0,10000):
+            if not head.next:
+                return False
+            head = head.next
+        return True
